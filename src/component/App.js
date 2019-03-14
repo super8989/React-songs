929261d6e370c6e7684c50946d87898a8ec60a0e
Sam <super8989@gmail.com>
2019-03-13 21:41:39 -0400

    connect function

--- src/component/App.js
+++ src/component/App.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import { selectSong } from '../actions';
+import React from "react";
+import SongList from "./SongList";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <SongList />
+    </div>
+  );
 };
 
 export default App;
