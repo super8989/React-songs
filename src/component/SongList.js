dbf94251f13ea08d78c1c7e162137ece57fc4c70
Sam <super8989@gmail.com>
2019-03-13 22:25:59 -0400

    calling actions creators from components

--- src/component/SongList.js
+++ src/component/SongList.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends React.Component {
   renderList() {
@@ -7,7 +8,12 @@ class SongList extends React.Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
 
           <div className="content">{song.title}</div>
@@ -24,7 +30,12 @@ class SongList extends React.Component {
 
 //take our state object (all data inside our redux store), run some calculation on it to cause it to show as props inside our component
 const mapStateToProps = state => {
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+//{ selectSong: selectSong }
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
