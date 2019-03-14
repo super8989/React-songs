84e8f8655f265692a455056217159ca461e70cd0
Sam <super8989@gmail.com>
2019-03-13 21:58:03 -0400

    configuring connect with MapStateToProps

--- src/component/SongList.js
+++ src/component/SongList.js
@@ -3,8 +3,15 @@ import { connect } from 'react-redux';
 
 class SongList extends React.Component {
   render() {
+    console.log(this.props);
+    //this.props === { songs:state.songs }
     return <div>SongList</div>;
   };
 }
 
-export default connect()(SongList);
+//take our state object (all data inside our redux store), run some calculation on it to cause it to show as props inside our component
+const mapStateToProps = state => {
+  return { songs:state.songs };
+}
+
+export default connect(mapStateToProps)(SongList);
